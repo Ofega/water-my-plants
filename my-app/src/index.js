a14ad8996f7b670718518b9668a7c13259991bef
Lindsey Cason <lindseyacason@gmail.com>
2019-09-24 13:05:17 -0500

    attempting to start react scripts

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,16 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import  { plantReducer } from "./reducers";
 
 import App from "./App";
 
 
+const store = createStore(
+    plantReducer, applyMiddleware(thunk)
+);
 
 ReactDOM.render(
+    
     <Provider store = { store }>
     <Router>   
-
         <App />
-    </Router>,
-document.getElementById('root'));
+    </Router>
+    </Provider>
+    
+,document.getElementById('root'));
 
