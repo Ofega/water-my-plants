e0a6de79a43ad9a11f561d3344fb14423e92389d
Lindsey Cason <lindseyacason@gmail.com>
2019-09-23 20:07:45 -0500

    added axiosWauth, added reducers, added actions, added private route, added some end points

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import FormikApp from "./App";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import reducer from "./reducers";
+
+const store = createStore(
+    reducer, applyMiddleware(thunk)
+);
+
 
 
 ReactDOM.render(
+    <Provider store = { store }>
     <Router>   
-<FormikApp />
-</Router>,
+    <FormikApp />
+    </Router>
+    </Provider>,
  document.getElementById('root'));
+
+
+ //CHECK FOR DEPENDENCIES YOU MIGHT BE MISSING
